529756449de42a875384cb15b9c67e8749f89279
khoirulariffin <khoirul.ariffin@ntxsolution.com>
2025-03-12 11:22:18 +0700

    add home page

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/copystation-admin/",
+  homepage: "https://khoirulariffin.github.io/copystation-admin/",
   server: {
     host: "::",
     port: 8080,
